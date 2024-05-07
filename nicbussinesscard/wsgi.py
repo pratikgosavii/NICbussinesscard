55b7814f8641fdca29e42f242a55a44f6c8b1f91
pratikgosavii <pratikgosavi654@gmail.com>
2024-05-07 20:45:17 +0000

    sdsD

--- nicbussinesscard/wsgi.py
+++ nicbussinesscard/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for nicbussinesscard project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# assuming your Django settings file is at
+# '/home/myusername/mysite/mysite/settings.py'
+path = '/home/bussinessnicecard/NICbussinesscard'
+if path not in sys.path:
+     sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'nicbussinesscard.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nicbussinesscard.settings')
-
 application = get_wsgi_application()
